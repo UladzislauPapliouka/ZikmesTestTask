967921d58c6dc5736934c945983d9ef66b7211e1
Uladzislau Papliouka <uladzislau.papliouka@gmail.com>
2022-11-15 17:10:56 +0300

    Added basic validation

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,29 @@
 import "./SCSS/style.scss"
+
+const AlertMessage = document.createElement("div")
+AlertMessage.classList.add("alert")
+
+const btn = document.querySelector("button")
+
+btn.addEventListener("click", ()=>{
+    validationHandler()
+})
+
+const validationHandler = () => {
+    const input = document.querySelector("#input") as HTMLInputElement
+    if(input.value.trim() === ""){
+        sendAlertMessage("Field must be filled...")
+    }
+}
+
+const sendAlertMessage =(message:string) => {
+    AlertMessage.innerText = message
+    document.querySelector("body").append(AlertMessage)
+    setTimeout(()=>AlertMessage.classList.toggle("active"),0)
+    setTimeout(()=>AlertMessage.classList.toggle("active"),1000)
+}
+
+AlertMessage.addEventListener("animationend", ()=>{
+    AlertMessage.remove()
+})
+
